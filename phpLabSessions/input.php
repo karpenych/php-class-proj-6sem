fbe035e4a80407a959b6917f8516a1c38c80b31c
karpenych <Dmitriy52444@gmail.com>
2023-04-05 18:36:54 +1000

    Add lab_Sessions

---
+++ phpLabSessions/input.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php
+    include "./inc/lib.inc";
+    session_start();
+    $page = "input";
+    $head = "<h2>Ввод параметров</h2>";
+?>
+<html>
+<head>
+    <title>input</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
+</head>
+<body>
+<?php
+    include "./inc/str.inc";
+?>
+</body>
+</html>
