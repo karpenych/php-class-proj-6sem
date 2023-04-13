dd77ac0ec1c54b378670c335ec91bd6fe8e65be8
karpenych <Dmitriy52444@gmail.com>
2023-04-13 21:27:56 +1000

    add lab_class

---
+++ phpLabClass/vegetables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<?php
+    include "./inc/lib.inc";
+    $page = "vegetables";
+    $head = "<h2 style=\"color: #001b09;\">Овощи</h2>";
+?>
+<html>
+<head>
+    <title>vegetables</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
+</head>
+<body>
+<?php
+    include "./inc/str.inc";
+?>
+</body>
+</html>
