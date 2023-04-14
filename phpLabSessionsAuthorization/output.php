144d5557397c8e635c31462ead3bfe38531b491a
karpenych <Dmitriy52444@gmail.com>
2023-04-14 19:53:29 +1000

    add lab_Authorization

---
+++ phpLabSessionsAuthorization/output.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php
+    include "./inc/lib.inc";
+    session_start();
+    $page = "output";
+    $head = "<h2>Вывод параметров</h2>";
+?>
+<html>
+<head>
+    <title>output</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
+</head>
+<body>
+<?php
+    include "./inc/str.inc";
+?>
+</body>
+</html>
